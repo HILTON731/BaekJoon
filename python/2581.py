3989ef45eb7bd10719d21b634647af717380dd35
HILTON731 <hilton3180@gmail.com>
2020-12-28 16:07:48 +0900

    math problem

---
+++ python/2581.py
@@ -0,0 +1,22 @@
+import sys
+
+start = int(sys.stdin.readline())
+end = int(sys.stdin.readline())
+
+minimum = end + 1
+tmp = 0
+
+for i in range(start, end + 1):
+    prime = True
+    for j in range(2, i):
+        if i % j == 0:
+            prime = False
+            break
+    if prime:
+        tmp += i
+        minimum = i if i < minimum else minimum
+
+if tmp == 0: print(-1)
+else:
+    print(tmp)
+    print(minimum)
