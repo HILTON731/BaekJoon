89b030ff01ad16a09030d6b4b68332fa1eccd3ce
HILTON731 <hilton3180@gmail.com>
2021-01-04 00:01:52 +0900

    solved

--- python/14889.py
+++ python/14889.py
@@ -7,31 +7,31 @@ for _ in range(size):
 min_size = sys.maxsize
 team_a = [i for i in range(1, size)]
 team_b = [0]
-print(team_a, team_b)
 
+# print(num_list)
 def potential():
-    res = 0
+    # print('potential',team_a, team_b)
+    res_a = 0
     for i in team_a:
         for j in team_a:
             if i < j:
-                res += (num_list[i][j] + num_list[j][i])
-            else:
-                break
+                res_a += (num_list[i][j] + num_list[j][i])
+
+    res_b = 0
     for i in team_b:
         for j in team_b:
             if i < j:
-                res -= (num_list[i][j] + num_list[j][i])
-            else:
-                break
-    print('potential', res)
-    return res
+                res_b += (num_list[i][j] + num_list[j][i])
+
+    # print('potential', res_a, res_b, abs(res_a - res_b))
+    return abs(res_a - res_b)
             
 def tracking(count):
     global min_size
     if count == size // 2 - 1:
         tmp = potential()
         min_size = min(min_size, tmp)
-        print(min_size, team_a, team_b)
+        # print(min_size, team_a, team_b)
         return
     else:
         for i in team_a:
@@ -43,6 +43,7 @@ def tracking(count):
                 
                 team_a.append(i)
                 team_b.remove(i)
+                team_a.sort()
 
 tracking(0)
 print(min_size)
