78c957a6b8f6d6f2d6b1018d5bdbf9530771980b
HILTON731 <hilton3180@gmail.com>
2021-01-06 03:36:07 +0900

    dp with ref

--- python/1463.py
+++ python/1463.py
@@ -1,46 +1,29 @@
-# X = int(input())
-# count = 0
-# result = [X]
-# def calculation(x):
-#     lst = []
-#     for i in x:
-#         lst.append(i-1)
-#         if i%3 ==0:
-#             lst.append(i/3)
-#         if i%2 ==0:
-#             lst.append(i/2)
-#     lst = set(lst)
-#     lst = list(lst)
-#     return lst
-# while True:
-#     if X == 1:
-#         break
-#     temp = result
-#     result = []
-#     result = calculation(temp)
-#     count += 1
-#     if min(result) == 1:
-#         break
-# print(count)
+import sys
 
-n = int(input())
-result = [n]
+num = int(sys.stdin.readline())
+dp = [num]
 count = 0
-def counter(numbers):
-    lst = list()
-    for num in numbers:
-        lst.append(num-1)
-        if num % 3 == 0:
-            lst.append(num/3)
-        if num % 2 == 0:
-            lst.append(num/2)
-    lst = set(lst)
-    lst = list(lst)
-    return lst
+
+def operation(num_list):
+    tmp = list()
+    for i in num_list:
+        tmp.append(i-1)
+        if not i % 3:
+            tmp.append(i / 3)
+        if not i % 2:
+            tmp.append(i / 2)
+    return tmp
 
 while True:
-    if min(result) == 1:
+    if 1 in dp:
         print(count)
         break
-    result = counter(result)
+    dp = operation(dp)
     count += 1
+
+# many ways of using memories.
+# in this case you can't know that which operation is best.
+# so must operate all cases.
+# this is another way of making function in dynamic programming problems.
+# could use every case of operation in memory and check is there a result.
+# in this case memory role in storage of operations so it works.
