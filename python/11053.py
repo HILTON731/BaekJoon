1b60feafe1315c61f97ba672c05f9f26cac1b7db
HILTON731 <hilton3180@gmail.com>
2021-01-07 22:08:31 +0900

    need to study more

---
+++ python/11053.py
@@ -0,0 +1,12 @@
+import sys
+
+# num = int(sys.stdin.readline())
+# tmp = list(map(int, sys.stdin.readline().split()))
+# count = [0] * num
+
+# for i in range(num):
+#     for j in range(i):
+#         if tmp[i] > tmp[j] and count[i] < count[j]:
+#             count[i] = count[j]
+#     count[i] += 1
+# print(max(count))
